c077fbd8d42e41686d5a55e1d5766976276aed04
shun <su791213t@hotmail.com>
2025-09-06 21:51:47 -0700

    finished DataInvestigator.py

--- DataInvestigator.py
+++ DataInvestigator.py
@@ -1,11 +1,40 @@
+import pandas as pd
+
+
 class DataInvestigator:
+    def __init__(self, df):
+        self.df = df
+        if not isinstance(df, pd.DataFrame):
+            self.df = None
 
-    def baseline(col):
-        pass
+    def baseline(self, col):
+        if self.df is None or not len(self.df.columns) > col >= 0: return None
 
-    def corr(col1, col2):
-        pass
+        try:
+            return self.df.iloc[:, col].mode().tolist()[0]
+        except:
+            return None
 
-    def zeroR(col):
-        pass
+    def corr(self, col1, col2):
+        if self.df is None: return None
 
+        if (not len(self.df.columns) > col1 >= 0 or
+            not len(self.df.columns) > col2 >= 0):
+            return None
+
+        try:
+            return self.df.iloc[:, col1].corr(self.df.iloc[:, col2])
+        except:
+            return None
+
+    def zeroR(self, col):
+        return self.baseline(col)
+
+
+
+
+df = pd.read_csv('gallstone.csv')
+di = DataInvestigator(df)
+print(di.baseline(1))
+print(di.zeroR(1))
+print(di.corr(2, 3))
